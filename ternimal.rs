1940382dde6a311b07106ad167e4395e5b989593
Octavian <ccraciun@gmail.com>
2018-11-01 03:13:02 -0700

    Add duration parameter.

--- ternimal.rs
+++ ternimal.rs
@@ -92,6 +92,8 @@ fn main() {
     arg_var!(speed, 30.0, 0.0, 1000.0);
     // Animation frames per second
     arg_var!(fps, 30.0, 0.1, 600.0);
+    // Duration of model simlation in seconds (if this is less than 0, run forever)
+    arg_var!(duration, -1.0);
 
     // Coloration gradient of the model, from its spine (`0`) to its outline (`1`)
     arg_var!(gradient, Gradient(vec![
@@ -250,6 +252,10 @@ fn main() {
 
         // Hide cursor while printing canvas to avoid flickering
         print!("\x1B[?25l{}\x1B[?25h\n", output);
+
+        if duration >= 0.0 && time > duration {
+            break;
+        }
 
         // Sleep to compensate for difference between rendering time and frame time
         let sleep_time = (1.0 / fps) - (seconds(start_time.elapsed()) - time);
